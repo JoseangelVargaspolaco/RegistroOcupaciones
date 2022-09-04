92b2933b24315979b17cec18ffd94d630ae7a2d9
JoseangelVargaspolaco <97201605+JoseangelVargaspolaco@users.noreply.github.com>
2022-09-03 21:41:42 -0400

    RegistroOcupaciones
    Registro de ocupaciones

---
+++ RegistroOcupacion/Entidades/Ocupaciones.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroOcupacion.Entidades
+{
+    public class Ocupaciones
+    {
+
+        [Key]
+        public int OcupacionId { get; set; }
+        public string? Descripcion { get; set; }
+        public double Salario  { get; set; }
+    }
+}
