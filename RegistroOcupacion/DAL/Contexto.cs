b2244adfcb9c2c74b12e76f8ce40800801573486
JoseangelVargaspolaco <97201605+JoseangelVargaspolaco@users.noreply.github.com>
2022-09-11 09:45:17 -0300

    Reparacion_archivos
    Reparacion de archivos

---
+++ RegistroOcupacion/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroOcupacion.Entidades;
+using RegistroOcupacion.DAL;
+
+namespace RegistroOcupacion.DAL{
+public class Contexto : DbContext
+{
+    public DbSet<Ocupaciones> Ocupaciones { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+}
+}
+ 
